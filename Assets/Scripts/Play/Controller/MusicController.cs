df984cf2aea480c7dba2be6f89f642955caf062a
CaptainBulba <varavaivan1@gmail.com>
2022-05-19 19:50:10 +0300

    updated game sounds

--- Assets/Scripts/Play/Controller/MusicController.cs
+++ Assets/Scripts/Play/Controller/MusicController.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicController : MonoBehaviour
 {
     public float maxVolume;
     private float soundVolume;
-    
+
     private bool soundToggle = true;
 
     private AudioSource audioSource;
+
+    public AudioClip playMusic;
+    public AudioClip gameoverMusic;
+
+    private string playScene = "Play";
+    private string gameoverScene = "Gameover";
 
     void Start()
     {
@@ -32,5 +39,16 @@ public class MusicController : MonoBehaviour
         soundToggle = !soundToggle;
 
         audioSource.volume = soundVolume;
+    }
+
+    public void ChangeMusic()
+    {
+        string levelName = SceneManager.GetActiveScene().name;
+        AudioClip music;
+
+        if (levelName == playScene) music = playMusic;
+        else music = gameoverMusic;
+
+        audioSource.clip = music;
     }
 }
